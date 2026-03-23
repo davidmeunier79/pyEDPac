030abe794008a2d2d53fa22fbd59b41597b92437
David Meunier <david.meunier@univ-amu.fr>
2026-03-23 17:28:40 +0100

    network tracer...

---
+++ src/edpac/tracer/network_tracer.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+from .tracer import Tracer
+
+class NetworkTracer(Tracer):
+    def __init__(self, network):
+        self.network = network
+
+        # Scaling parameters (adjust based on your neuron model constants)
+        #self.v_min = neuron.config.RESTING_POTENTIAL
+        #self.v_max = neuron.config.THRESHOLD_REF    # Usually the threshold
+
+        super().__init__()
+
+    def record(self, time, spike_neuron_ids):
+        """Samples the current membrane potential."""
+        # Shift history to the left and add new value
+
+        self.history.append((time, spike_neuron_ids)) # Accessing the potential of the EDNeuron
+
+    def plot(self, target_dir = 0):
+        """
+        Plotting with matplotlib
+        """
+
+        if not target_dir:
+            target_dir = os.path.abspath("")
+
+        if len(self.history)==0:
+            return
+
+        max_time = self.history[-1][0]
+
+        spike_array = np.zeros (shape = (max_time+1, self.network.get_nb_neurons()))
+
+        for time, spike_ids in self.history:
+            spike_array[time, spike_ids] = 1
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+
+        plt.imshow(spike_array.T, aspect='auto', cmap='binary', origin='lower')
+
+        plt.xlabel("Time Steps")
+        plt.ylabel("Neuron Index")
+        plt.title("Network Spiking Activity")
+#
+#     plt.tight_layout()
+#     plt.savefig(filename, dpi=300)
+#     plt.close() # Closes the figure to free up memory
+#
+        file_path = os.path.join(target_dir, "Network_spikes.eps")
+        print(file_path)
+
+        fig.savefig(file_path)
+        assert os.path.exists(file_path), \
+            "Error with plotting {}".format(plot_signals_file)
+
+
