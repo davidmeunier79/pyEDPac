74b3e05f4ee583f51f353b7ba37d54b63eaf32ba
David Meunier <david.meunier@univ-amu.fr>
2026-04-01 14:53:23 +0200

    population

--- src/multipac/parallel/parallel_zoo.py
+++ src/multipac/parallel/parallel_zoo.py
@@ -178,7 +178,7 @@ class ParallelZoo(Zoo):
         print(f"Predator {new_index=} available, building")
         if self._compute_online_reproduction(new_index, contact_index, pacman_index):
             #self.stats["nb_predators"] += 1
-            self.init_random_position(new_index)
+            self.population.init_random_position(new_index)
             self.nb_deads -= 1
 
         print(f"******************** {self.nb_deads=} ***********************")
