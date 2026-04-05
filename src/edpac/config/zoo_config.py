e20bd4800d1f7c4c4b855f16cfc222f5ab3eff3f
David Meunier <david.meunier@univ-amu.fr>
2026-04-05 19:45:23 +0200

    increase pacgum value

--- src/edpac/config/zoo_config.py
+++ src/edpac/config/zoo_config.py
@@ -32,7 +32,7 @@ class MultiPacmanConfig:
     NB_LIFE_POINTS_PER_PREY_BITE = 100
 
     # Special 2 populations prey and predators
-    NB_LIFE_POINTS_PER_PACGUM_PREY = 20
+    NB_LIFE_POINTS_PER_PACGUM_PREY = 50
     NB_LIFE_POINTS_PER_PACGUM_PREDATOR = 1
 
     MIN_LIFE_FOR_REPROD = 100
