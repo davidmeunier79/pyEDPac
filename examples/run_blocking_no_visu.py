df09bf9b75a1c8a582494622970edb017a885e7f
David Meunier <david.meunier@univ-amu.fr>
2026-04-15 10:40:10 +0200

    forgot to move run_non_blocking in clean_before_v0.2 better print

--- examples/run_blocking_no_visu.py
+++ examples/run_blocking_no_visu.py
@@ -100,9 +100,9 @@ def main(stats_path):
 
         nb_alive_indiv = len([pac for pac in zoo.population.individuals if pac])
 
-        print(f"******************** {nb_alive_indiv=} ***********************")
+        print(f" {nb_alive_indiv=} ")
 
-        print(f"nb_preys={zoo.stats["nb_preys"][-1]} nb_predators={zoo.stats["nb_predators"][-1]} mean_prey_fitness={zoo.stats["mean_prey_fitness"][-1]} mean_predator_fitness={zoo.stats["mean_predator_fitness"][-1]} generation={zoo.stats["generation"][-1]}, nb_deads={zoo.stats["nb_deads"][-1]}, nb_added_pacgums={zoo.stats["nb_added_pacgums"][-1]}")
+        print(f" nb_preys={zoo.stats["nb_preys"][-1]} \n nb_predators={zoo.stats["nb_predators"][-1]} \n mean_prey_fitness={zoo.stats["mean_prey_fitness"][-1]} \n mean_predator_fitness={zoo.stats["mean_predator_fitness"][-1]} \n generation={zoo.stats["generation"][-1]} \n nb_deads={zoo.stats["nb_deads"][-1]} \n nb_added_pacgums={zoo.stats["nb_added_pacgums"][-1]}")
 
         if nb_alive_indiv == 0:
             print("All individuals are dead , Breaking")
