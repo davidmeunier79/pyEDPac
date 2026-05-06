89f913b9af68c804c2e787e074e3905ca88f6086
David Meunier <david.meunier@univ-amu.fr>
2026-05-06 14:25:20 +0200

    25

--- src/edpac/config/ga_config.py
+++ src/edpac/config/ga_config.py
@@ -84,7 +84,7 @@ class PopulationConfigMulti(BaseConfig):
     # # # # Taille population
     POPULATION_SIZE: int = 63
 
-    INIT_PREY_POPULATION_SIZE : int = 20
+    INIT_PREY_POPULATION_SIZE : int = 25
     INIT_PREDATOR_POPULATION_SIZE : int = 20
 
 
