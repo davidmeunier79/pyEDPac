ecc3995efffc942133ab0469e4e36cfae10823f3
David Meunier <david_meunier_79@hotmail.fr>
2026-05-18 14:33:46 +0200

    Add ed_network package initialization

---
+++ src/edpac/ed_network/__init__.py
@@ -0,0 +1,13 @@
+"""
+edpac.ed_network - Event-Driven Neural Network Components
+"""
+
+from .ed_neuron import EDNeuron
+from .ed_synapse import EDSynapse
+from .evo_network import EvoNetwork
+
+__all__ = [
+    "EDNeuron",
+    "EDSynapse",
+    "EvoNetwork",
+]
